d281ac071f8c43ab3706a986be45a644401734fc
app[le <apple@apples-MacBook-Pro.local>
2023-11-20 13:05:53 +0530

    hsvhh

---
+++ build/precache-manifest.d862bb5fcdb7c04a8846357eb4bd04b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68420e8717656d3ec1c141fe292e3f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515ba2607cf5e1f73ce4",
+    "url": "/static/css/2.83e47d14.chunk.css"
+  },
+  {
+    "revision": "13d628d7b2412116390d",
+    "url": "/static/css/main.f9a35af2.chunk.css"
+  },
+  {
+    "revision": "515ba2607cf5e1f73ce4",
+    "url": "/static/js/2.0f366fe4.chunk.js"
+  },
+  {
+    "revision": "6f9da7f7ca4d38d99e32",
+    "url": "/static/js/3.3c67e748.chunk.js"
+  },
+  {
+    "revision": "13d628d7b2412116390d",
+    "url": "/static/js/main.fe8dad1d.chunk.js"
+  },
+  {
+    "revision": "eb72a3a0837ee0ea4a5c",
+    "url": "/static/js/runtime~main.6cd54f33.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);

648280cc338ad9c0d280f54db8fa73b943cb2bfa
Aquib479 <mdaquib479@gmail.com>
2023-11-21 19:34:53 +0530

    changes

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const { connectToMongoDB } = require('./DB/connection')
-// connections ans listeners
-const port = process.env.PORT || 5000;
-connectToMongoDB()
-    .then(() => {
-        app.listen(port, () => console.log("server has started & connected to DataBase"));
-    })
-    .catch((err) => console.log(err));
